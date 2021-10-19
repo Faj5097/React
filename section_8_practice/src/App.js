5a08d283480df981190840d7f838180be3963505
Faj5097 <fab.jan@t-online.de>
2021-10-19 16:14:33 +0200

    Add and Show Users as List

--- section_8_practice/src/App.js
+++ section_8_practice/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import AddUsers from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, { name: uName, age: uAge, id: Math.random() }];
+    });
+  };
+
   return (
     <div>
-
+      <AddUsers onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
