7748df993516c28f4e889e4e9feb32f447acbd3a
Faj5097 <fab.jan@t-online.de>
2021-11-11 17:54:59 +0100

    Sign In and Sign Up with Firebase REST API

---
+++ section_22_authentification/src/components/Auth/AuthForm.js
@@ -0,0 +1,93 @@
+import { useCallback, useRef, useState } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const signInOrUp = useCallback(async (url) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: emailInputRef.current.value,
+          password: passwordInputRef.current.value,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setIsLoading(false);
+      if (response.ok) {
+        console.log(data);
+      } else {
+        const errorMessage = "Authenfication failed";
+        throw new Error(errorMessage);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }, []);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (isLogin) {
+      signInOrUp(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBEuSQS8Fw_MiY-0hQob_ziGRgPHfPO-V4"
+      );
+    } else {
+      signInOrUp(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBEuSQS8Fw_MiY-0hQob_ziGRgPHfPO-V4"
+      );
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
